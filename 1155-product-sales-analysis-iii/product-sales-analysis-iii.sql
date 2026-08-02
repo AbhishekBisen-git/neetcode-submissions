2476fae508de7c752cfad5761c2ffe00e7953f56
AbhishekBisen <abhishekbisen.work@gmail.com>
2026-08-02 21:47:23 +0530

    Time: 694 ms (79.66%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,22 +1,23 @@
--- select 
--- A.product_id,
--- A.year as first_year,
--- A.quantity as quantity , 
--- A.price as price
--- from sales A left join ( select product_id , min(year) as year from sales group by product_id) B on A.product_id = B.product_id and A.year = B.year
--- group by A.year , A.product_id
+ select 
+ A.product_id,
+ A.year as first_year,
+ A.quantity as quantity , 
+ A.price as price
+ from sales A 
+  join ( select product_id , min(year) as year from sales group by product_id) B on A.product_id = B.product_id and A.year = B.year
 
 
 
-SELECT s.product_id,
-       s.year AS first_year,
-       s.quantity,
-       s.price
-FROM Sales s
-JOIN (
-    SELECT product_id, MIN(year) AS first_year
-    FROM Sales
-    GROUP BY product_id
-) t
-ON s.product_id = t.product_id 
-AND s.year = t.first_year;
+
+-- SELECT s.product_id,
+--        s.year AS first_year,
+--        s.quantity,
+--        s.price
+-- FROM Sales s
+-- JOIN (
+--     SELECT product_id, MIN(year) AS first_year
+--     FROM Sales
+--     GROUP BY product_id
+-- ) t
+-- ON s.product_id = t.product_id 
+-- AND s.year = t.first_year;
