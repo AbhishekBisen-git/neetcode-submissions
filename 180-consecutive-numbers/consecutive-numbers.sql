9d1758883605882b810743df87e6d95425619c25
AbhishekBisen <abhishekbisen.work@gmail.com>
2026-08-04 22:46:45 +0530

    Time: 743 ms (13.66%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,11 +1,9 @@
-SELECT DISTINCT num AS ConsecutiveNums
-FROM (
-    SELECT
-        id,
-        num,
-        LAG(num, 1) OVER (ORDER BY id) AS previous_num,
-        LAG(num, 2) OVER (ORDER BY id) AS two_rows_before
-    FROM Logs
-) t
-WHERE num = previous_num
-  AND num = two_rows_before;
+select b.n3 as ConsecutiveNums  from
+(select DISTINCT (case when a.num=a.n1 and a.n1=a.n2 then a.num else null end) as n3
+from
+
+(select num,
+lead(num,1) over (order by id) as n1,
+lead(num,2) over (order by id) as n2
+from logs) a)b
+WHERE b.n3 is not null
