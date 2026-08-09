db141319720bd5894fae0e7dd9c695e3169c91ba
AbhishekBisen <abhishekbisen.work@gmail.com>
2026-08-09 14:09:43 +0530

    Time: 483 ms (73.56%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,9 +1,15 @@
-SELECT id,
+select  
 
-    CASE 
-        WHEN p_id IS NULL THEN 'Root'
-        WHEN id IN (SELECT p_id FROM Tree)THEN 'Inner'
-        ELSE 'Leaf'
-        END AS type
- FROM Tree
-		
+id , 
+case
+when  p_id is null then 'Root' 
+when id not in ( select distinct p_id from tree) then 'Leaf' 
+when id in ( select distinct p_id from tree) then 'Inner' 
+else 'Leaf'
+
+end as type
+
+
+
+from
+tree
